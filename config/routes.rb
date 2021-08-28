a3079ecc0b726f96daa0024df681595a997dd373
odajunpei <i.penju1022@gmail.com>
2021-08-28 05:12:55 +0000

    [Start]

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
+end
